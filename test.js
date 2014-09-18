2c0c846b74bfab56633918acfce38999e6d0df89
Jarvys <yuhan534@126.com>
2014-09-19 01:58:14 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var ensureRange = require('./index');
+
+describe('ensure-range', function() {
+	it('[1, 3]', function() {
+		assert.equal(ensureRange(1, 3, 3), 3);
+		assert.equal(ensureRange(1, 3, 1), 1);
+		assert.equal(ensureRange(1, 3, 2), 2);
+		assert.equal(ensureRange(1, 3, -1), 1);
+		assert.equal(ensureRange(1, 3, 4), 3);
+	});
+
+	it('[1, 1]', function() {
+		assert.equal(ensureRange(1, 1, 3), 1);
+		assert.equal(ensureRange(1, 1, 1), 1);
+	});
+
+	it('[1, -1]', function() {
+		assert.throws(function() {
+			ensureRange(1, -1, 3)
+		}, function(err) {
+			return true;
+		});
+	});
+
+});
